80f49cf38cb2450c9da00c3b6fff3d40663058ee
Woodrat <95650272+Woodratius@users.noreply.github.com>
2024-12-03 21:42:43 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+name = "Woodratius"
+age = 40
+
+print("My mame is ", name, ", ", "I'm ", age, " years old", sep="")
+print(f'My name is {name}, I am {age} years old')
+
+quantity_of_completed_homeworks = 12
+quantity_of_spent_time = 1.5
+Course = "Python"
+
+print(f'Курс {Course}, всего задач: {quantity_of_completed_homeworks}, затрачено часов: {quantity_of_spent_time}, среднее время выполнения {quantity_of_spent_time / quantity_of_completed_homeworks}.')
